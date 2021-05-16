65692fec84dcd2bfdd6575b1d65b8a6ec5612854
moses1206 <mose23@naver.com>
2021-05-16 18:15:56 +0900

    2

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route   POST    api/users
 // @desc    Register a user
 // @access  public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [body("name", "name is required").not().isEmpty()],
+  (req, res) => {
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
